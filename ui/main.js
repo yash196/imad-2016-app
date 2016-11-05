bfc89b08be32cd1a51f8b5f81c7f3a38eca3b185
yash196 <y.gupta196@gmail.com>
2016-11-05 18:27:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML="New value";
 //Move the image
-var img=document.getElementById('img');
+var img=document.getElementById('madi');
 img.onclick=function(){
     img.style.marginleft='100px';
 };
